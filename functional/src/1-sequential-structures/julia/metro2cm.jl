3fa608697a35b53ef2798dcaa96111fe36cf36c7
sswellington <wellws@pm.me>
2021-06-27 00:00:34 -0300

    rm sequential structures

--- functional/src/1-sequential-structures/julia/metro2cm.jl
+++
@@ -1,7 +0,0 @@
-###  Converter unidades
-# Faça um programa que converta uma medida de metros para centímetros. 
-
-println("informe um valor em metros ")
-convert = ( parse(UInt8, readline()) * 100)
-
-println(convert," cm")
--- functional/src/1-sequential-structures/python/quadrado.py
+++
@@ -1,2 +0,0 @@
-side = int(input("Digite o valor correspondente ao lado de um quadrado: "))
-print('perímetro: ', side*4,'- área: ',side**2)
